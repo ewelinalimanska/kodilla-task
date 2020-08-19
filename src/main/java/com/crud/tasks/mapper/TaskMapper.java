3eabb680051f4b3dd3a1818358a43ce4251fae76
ewelinalimanska <limanskaewelina@gmail.com>
2020-08-19 20:09:32 +0200

    Modul 18-19

---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapper {
+
+    public Task mapToTask(final TaskDto taskDto) {
+        return new Task(
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getContent());
+    }
+
+    public TaskDto mapToTaskDto(final Task task){
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getContent());
+    }
+
+
+}
