eb928fd1cb2c224ab13fae64dc9cf38b7cf03c4a
ewelinalimanska <limanskaewelina@gmail.com>
2020-08-21 12:38:07 +0200

    Modul 18-19

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks(){
@@ -31,8 +41,9 @@ public class TaskController {
         return new TaskDto(1L, "test title", "test_content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTask")
-    public void createTask(TaskDto taskDto){
+    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto){
+        service.saveTask(taskMapper.mapToTask(taskDto));
     }
 
 }
--- src/main/java/com/crud/tasks/mapper/TaskMapper.java
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -4,10 +4,13 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class TaskMapper {
 
-    public Task mapToTask(final TaskDto taskDto) {
+    public Task mapToTask(final TaskDto taskDto){
         return new Task(
                 taskDto.getId(),
                 taskDto.getTitle(),
@@ -21,5 +24,10 @@ public class TaskMapper {
                 task.getContent());
     }
 
+    public List<TaskDto> mapToTaskDtoList(final List<Task> taskList){
+        return taskList.stream()
+                .map(t -> new TaskDto(t.getId(), t.getTitle(), t.getContent()))
+                .collect(Collectors.toList());
+    }
 
 }
