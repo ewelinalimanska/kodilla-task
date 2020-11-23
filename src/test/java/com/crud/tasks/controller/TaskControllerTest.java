aabb958a4be4ec989d92c257250242aa3e53581c
ewelinalimanska <limanskaewelina@gmail.com>
2020-11-23 19:13:47 +0000

    TestTaskController

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -18,8 +18,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -83,15 +86,54 @@ class TaskControllerTest {
 
         Gson gson = new Gson();
         String jsonContent = gson.toJson(taskDto);
+
         //when&then
-        mockMvc.perform(put("v1/task/updateTask")
+        mockMvc.perform(put("/v1/task/updateTask")
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
                 .content(jsonContent))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.title", is("test_dto")))
-                .andExpect(jsonPath("$.id", is("1")))
+                .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.content", is("test_dto")));
+    }
+
+    @Test
+    public void testCreateTask() throws Exception {
+
+        //given
+        TaskDto taskDto = new TaskDto(1L, "dto_test", "dto_content");
+
+        Task task = new Task(1L, "task_test", "content_test");
+
+        when(dbService.saveTask(taskMapper.mapToTask(taskDto))).thenReturn(task);
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+
+        //when&then
+        mockMvc.perform(post("/v1/task/createTask")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk());
+        verify(dbService).saveTask(any());
+        assertThat(task.getTitle()).isEqualTo("task_test");
+    }
+
+    @Test
+    public void testDeleteTask() throws Exception {
+
+        Task task = new Task(1L, "test_task", "test_content");
+
+        when(dbService.getTask(task.getId())).thenReturn(Optional.of(task));
+
+        //when&then
+        mockMvc.perform(delete("/v1/task/deleteTask?taskId=1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("id", "1"))
+                .andExpect(status().isOk());
+        verify(dbService).deleteTask(task.getId());
     }
 
 
